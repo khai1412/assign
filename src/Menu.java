3a9d7c4fa32876f89c6d23e305817174bf1d04e8
khai1412 <khai1412@gmail.com>
2021-11-01 22:10:18 +0700

    ham brand

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,7 @@
+
+import static java.lang.Math.E;
+import java.util.ArrayList;
+import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,23 @@
  * @author admin
  */
 public class Menu {
-    
+    int int_getChoice(ArrayList<Brand> options){
+        Scanner sc= new Scanner(System.in);
+        for(int i=0;i<options.size();i++){
+            System.out.println((i+1)+" "+options.get(i));
+        }
+        System.out.print("Please choose an option 1 to N: ");
+        int p=sc.nextInt();
+        return p;
+    }
+    Brand ref_getChoice(ArrayList<Brand> options){
+        Scanner sc= new Scanner(System.in);
+        int choice =this.int_getChoice(options);
+        int n=options.size();
+        int p;
+        do{
+            p=sc.nextInt();
+        } while(p<0||p>n);
+        return options.get(p);
+    }
 }
